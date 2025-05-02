74ad79b273bfdadb357a15a53c3ec080bc55339d
SATYAMRAJ2003 <satyamraj457@gmail.com>
2025-05-02 14:04:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/FoodApp/', 
+  //base: '/FoodApp/', 
   plugins: [
     
     tailwindcss(),
